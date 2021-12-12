4c60e3eb6cd2b1ae45fc5ec891d85a7503b703f9
Daiji TSUTSUI <daiji.tsutsui417@gmail.com>
2021-12-13 02:00:22 +0900

    Add scheduler

---
+++ lib/scheduler.rb
@@ -0,0 +1,47 @@
+require 'yaml'
+
+class Scheduler
+  attr_accessor :next
+
+  def initialize
+    input = open('schedule.yaml', 'r') { |f| YAML.load(f) }
+    @start = input["start"]
+    @end = input["end"]
+    @table = [@start]
+
+    indicator = @start.clone
+    rule = input["rule"].shift
+    while true do
+      if indicator > rule["until"]
+        rule = input["rule"].shift
+        break if rule.nil?
+        next
+      end
+      indicator += rule["freq"]
+      break if indicator > @end
+      @table.push indicator
+    end
+    @next = @table.shift
+  end
+
+  def wait
+    return if is_finished
+    while true do
+      break if is_on_fire
+      sleep 10
+    end
+  end
+
+  def is_on_fire
+    return false if is_finished
+    if Time.now > @next
+      @next = @table.shift
+      return true
+    end
+    false
+  end
+
+  def is_finished
+    Time.now > @end
+  end
+end
--- main.rb
+++ main.rb
@@ -8,16 +8,23 @@ end
 
 Capybara.default_driver = :selenium_chrome
 
+scheduler = Scheduler.new
+
 Capybara::Session.new(:selenium_chrome).tap { |session|
-  top_page = Jra::TopPage.new
-  top_page.load
-
-  odds_page = top_page.go_odds
-  race_odds_page = odds_page.go_course(Jra::SUNDAY, '阪神')
-
-  single_odds_page = race_odds_page.go_single_odds(Jra::RACE_11)
-  pp single_odds_page.get_uma_name
-  pp single_odds_page.get_tan_odds
-  pp single_odds_page.get_fuku_odds
-  sleep 5
+  while true do
+    break if scheduler.is_finished
+    puts "Fire at #{Time.now}" if scheduler.is_on_fire
+    sleep 1
+  end
+  # top_page = Jra::TopPage.new
+  # top_page.load
+  #
+  # odds_page = top_page.go_odds
+  # race_odds_page = odds_page.go_course(Jra::SUNDAY, '阪神')
+  #
+  # single_odds_page = race_odds_page.go_single_odds(Jra::RACE_11)
+  # pp single_odds_page.get_uma_name
+  # pp single_odds_page.get_tan_odds
+  # pp single_odds_page.get_fuku_odds
+  # sleep 5
 }
