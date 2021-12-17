7674d96b26d18b6b7874b677eb4dff82552f056f
Daiji TSUTSUI <daiji.tsutsui417@gmail.com>
2021-12-18 06:35:08 +0900

    Add vanilla gradient update method

--- lib/odds_analyzer.rb
+++ lib/odds_analyzer.rb
@@ -3,44 +3,57 @@
 class OddsAnalyzer
   attr_accessor :t, :a, :b
   attr_accessor :ini_p
+  attr_accessor :model, :blueprint
+  attr_accessor :eps
 
   def initialize
     @a = [1.0]
-    @b = []
+    @b = [1.0]  # @b[0]はdummy
+    @eps = 0.00001
   end
 
   # mathematical model
   def forecast(odds_list)
     adjust_params(odds_list)
     p = @ini_p
-    model = [@ini_p]
-    (odds_list.size - 1).times do |i|
-      odds = odds_list[i]
-      a = @a[i+1]/@a.first(i+2).sum
+    @model = []
+    @blueprint = []
+    (1..odds_list.size - 1).each do |i|
+      odds = odds_list[i-1]
+      a = @a[i] / @a.first(i+1).sum
       b = @b[i]
       p = forecast_next(p, odds, @t, a, b)
-      model.push p
+      @model.push p
     end
-    model
+    @model
   end
 
   # t: true distribution
   # a: weight of importance at this moment
   # b: coefficient of certainty at this moment
   def forecast_next(prev, odds, t, a, b)
-    p = strategy(odds, t, b)
-    prev.map.with_index { |r, i| (1.0 - a) * r + a * p[i] }
+    q = strategy(odds, t, b)
+    @blueprint.push q
+    prev.map.with_index { |r, i| (1.0 - a) * r + a * q[i] }
   end
 
-  def loss(odds_list)
-    model = forecast(odds_list)
-    p_list = odds_list.map { |odds| odds_to_prob(odds) }
-    p_list.map.with_index { |p, k| kl_div(p, model[k]) }
+  def update_params(odds_list, prepare: false)
+    forecast(odds_list) if prepare
+    @model.each do |p|
+      update_a(p, odds_list)
+      update_b(p, odds_list)
+      update_t(p, odds_list)
+    end
+    @a = normalize(@a)
+    @t = normalize(@t)
   end
 
-  def odds_to_prob(odds)
-    w = odds.map { |v| 1.0/v }
-    normalize(w)
+  def loss(odds_list, prepare: false)
+    forecast(odds_list) if prepare
+    @model.map.with_index do |q, k|
+      p = odds_to_prob(odds_list[k+1])
+      kl_div(p, q)
+    end
   end
 
   def strategy(odds, t, b)
@@ -49,11 +62,18 @@ class OddsAnalyzer
   end
 
   def kl_div(p, q)
-    f = p.map.with_index { |pi, i| Math.log(pi) - Math.log(q[i]) }
+    f = p.map.with_index { |p_i, i| Math.log(p_i) - Math.log(q[i]) }
     expectation(p, f)
   end
 
+
   private
+
+    def odds_to_prob(odds)
+      w = odds.map { |v| 1.0/v }
+      normalize(w)
+    end
+
     def normalize(w)
       total = w.sum
       w.map { |r| r/total }
@@ -68,11 +88,86 @@ class OddsAnalyzer
       @ini_p ||= odds_to_prob(odds_list[0])
       @t ||= @ini_p.clone
       if @a.size != odds_list.size
-        ratio = @a.size.to_f/odds_list.size.to_f
+        ratio = @a.size.to_f / odds_list.size.to_f
         @a.map! { |ai| ai * ratio }
-        @a += Array.new(odds_list.size - @a.size, 1.0/odds_list.size.to_f)
+        @a += Array.new(odds_list.size - @a.size, 1.0 / odds_list.size.to_f)
         @a = normalize @a
       end
-      @b += Array.new(odds_list.size - @b.size - 1, @b[-1] || 1.0)
+      @b += Array.new(odds_list.size - @b.size, @b[-1] || 1.0)
+    end
+
+    def update_a(p, odds_list)
+      da = grad_a(p, odds_list)
+      da.each.with_index(1) do |da_i, i|
+        @a[i] -= @eps * da_i
+        @a[0] += @eps * da_i
+      end
+    end
+
+    def update_b(p, odds_list)
+      db = grad_b(p, odds_list)
+      db.each.with_index(1) do |db_i, i|
+        @b[i] -= @eps * db_i
+      end
+    end
+
+    def update_t(p, odds_list)
+      dt = grad_t(p, odds_list)
+      dt.each.with_index do |dt_i, i|
+        @t[i] -= @eps * dt_i
+      end
+    end
+
+    def grad_a(p, odds_list)
+      @blueprint.map.with_index(1) do |strat, k|
+        teacher = odds_to_prob(odds_list[k])
+        grad_a_for_instant(p, teacher, strat)
+      end
+    end
+
+    def grad_a_for_instant(p, teacher, strat)
+      f = p.map.with_index { |p_i, i| (strat[i] - @ini_p[i]) / p_i }
+      -expectation(teacher, f)
+    end
+
+    def grad_b(p, odds_list)
+      @blueprint.map.with_index(1) do |strat, k|
+        teacher = odds_to_prob(odds_list[k])
+        grad_b_for_instant(p, teacher, strat, @a[k], odds_list[k])
+      end
+    end
+
+    def grad_b_for_instant(p, teacher, strat, a, odds)
+      f1 = @t.map.with_index { |ti, i| ti * odds[i] }
+      exp_f1 = expectation(strat, f1)
+      f2 = p.map.with_index { |p_i, i| a * strat[i] * (f1[i] - exp_f1) / p_i }
+      -expectation(teacher, f2)
+    end
+
+    def grad_t(p, odds_list)
+      grad_list = @blueprint.map.with_index(1) do |strat, k|
+        teacher = odds_to_prob(odds_list[k])
+        grad_t_for_instant(p, teacher, strat, @a[k], @b[k], odds_list[k])
+      end
+      grad_list.transpose.map { |row| row.sum }
+    end
+
+    def grad_t_for_instant(p, teacher, strat, a, b, odds)
+      result = []
+      odds.size.times do |j|
+        f = p.map.with_index do |p_i, i|
+          a * b * strat[i] * (
+            odds[j] * (delta(i, j) - strat[j])
+            - odds[0] * (delta(i, 0) - strat[0])
+          ) / p_i
+        end
+        result.push -expectation(teacher, f)
+      end
+      result
+    end
+
+    def delta(i, j)
+      return 1.0 if i == j
+      0.0
     end
 end
--- main.rb
+++ main.rb
@@ -13,27 +13,38 @@ fetcher = OddsFetcher.new(
   race: Jra::RACE_1,
 )
 # manager = DataManager.new('dummy1')
-manager = DataManager.new('test4')
+manager = DataManager.new('dummy2')
 analyzer = OddsAnalyzer.new
 
-logger.info "DataManager got data: #{manager.data}"
+# logger.info "DataManager got data: #{manager.data}"
 
-fetcher.odds = manager.data
-while true do
-  break if scheduler.is_finished
-  if scheduler.is_on_fire
-    fetcher.run
-    logger.info fetcher.log
-  end
-  sleep 1
-end
-manager.save
-
+# fetcher.odds = manager.data
 # while true do
-#
+#   break if scheduler.is_finished
+#   if scheduler.is_on_fire
+#     fetcher.run
+#     logger.info fetcher.log
+#   end
+#   sleep 1
 # end
+# manager.save
 
-odds_list = manager.odds
-p loss_list = analyzer.loss(odds_list)
-p analyzer.model
-logger.info "Total loss: #{loss_list.sum}"
+p odds_list = manager.odds
+puts "odds_list.size: #{odds_list.size}"
+start = Time.now
+1001.times do |t|
+  analyzer.forecast(odds_list)
+  analyzer.update_params(odds_list)
+  if t % 10 == 0
+    puts "Count: #{t}, Time elapsed: #{Time.now - start}"
+    puts "Loss: #{analyzer.loss(odds_list)}"
+    # puts "a: #{analyzer.a.sum}, t: #{analyzer.t.sum}"
+    # puts "a: #{analyzer.a}"
+    # puts "b: #{analyzer.b}"
+    # puts "t: #{analyzer.t}"
+  end
+end
+
+# p loss_list = analyzer.loss(odds_list)
+# p analyzer.model
+# logger.info "Total loss: #{loss_list.sum}"
